d87292a1f388990b5f15135c938ea702be6c03f8
TarasBlyznyuk <ipz222_btr@student.ztu.edu.ua>
2024-01-28 14:55:22 +0200

    Додано файли програми на C#

---
+++ AdditionProj/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+namespace AdditionProj;
+
+internal static class Program {
+    public static void Main() {
+        var sum = Enumerable.Range(0, 2).Sum(_ => GetNumFromInput());
+        Console.WriteLine($"Final number: {sum}");
+    }
+
+    private static int GetNumFromInput() {
+        Console.Write("Enter the number: ");
+        return int.TryParse(Console.ReadLine(), out var num) ? num : 0;
+    }
+}
